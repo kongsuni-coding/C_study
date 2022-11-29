23559584e19550e4f01a95a067c027039380bab9
kongsuni-coding <118699247+kongsuni-coding@users.noreply.github.com>
2022-11-29 17:02:56 +0900

    Create CH1_q4.c

---
+++ CH1/CH1_q4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int height;
+    scanf("%d",&height);
+    if (120<=height && height<=150)
+        printf("%s\n","YES");
+    else   
+        printf("%s\n","NO");
+    return 0;
+}
